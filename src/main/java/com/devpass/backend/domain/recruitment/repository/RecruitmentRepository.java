f4ee59f6eb7893d51d1a8f02aa0e827cf77dfb1b
hangyeol <snake011128@naver.com>
2025-03-19 00:37:23 +0900

    fix: findById 삭제

--- src/main/java/com/devpass/backend/domain/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/com/devpass/backend/domain/recruitment/repository/RecruitmentRepository.java
@@ -1,9 +1,7 @@
 package com.devpass.backend.domain.recruitment.repository;
 
 import com.devpass.backend.domain.recruitment.entity.Recruitment;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
-    Optional<Recruitment> findById(Long id);
 }
