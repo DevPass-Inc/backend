eb75944d8168cda706e306fb105020cea188b613
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-03-28 19:00:05 +0900

    feat: request 빌더 추가

--- src/main/java/com/devpass/backend/domain/devexperience/dto/request/DevExperienceAddRequest.java
+++ src/main/java/com/devpass/backend/domain/devexperience/dto/request/DevExperienceAddRequest.java
@@ -1,10 +1,12 @@
 package com.devpass.backend.domain.devexperience.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Builder
 public class DevExperienceAddRequest {
     private String title;
     private String description;
