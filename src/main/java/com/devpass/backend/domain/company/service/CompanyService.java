b2ccfac1b04fa23aa3daf3f5fc78ac982e829b62
hangyeol <snake011128@naver.com>
2025-03-29 15:34:41 +0900

    fix: 트랜잭션 추가

--- src/main/java/com/devpass/backend/domain/company/service/CompanyService.java
+++ src/main/java/com/devpass/backend/domain/company/service/CompanyService.java
@@ -1,10 +1,13 @@
 package com.devpass.backend.domain.company.service;
 
+import com.devpass.backend.domain.company.converter.CompanyConverter;
+import com.devpass.backend.domain.company.dto.response.CompanyDetailResponseDTO;
 import com.devpass.backend.domain.company.entity.Company;
 import com.devpass.backend.domain.company.exception.CompanyNotFoundException;
 import com.devpass.backend.domain.company.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,10 +15,11 @@ public class CompanyService {
     private final CompanyRepository companyRepository;
 
     // 기업 개별 조회
-    public Company getCompany(Long companyId) {
+    @Transactional(readOnly = true)
+    public CompanyDetailResponseDTO getCompanyById(Long companyId) {
         Company company = companyRepository.findById(companyId)
             .orElseThrow(CompanyNotFoundException::new);
 
-        return company;
+        return CompanyConverter.toCompanyDetailResponse(company);
     }
 }
