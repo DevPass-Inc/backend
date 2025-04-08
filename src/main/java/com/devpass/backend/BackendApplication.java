50fcc47dc3ef26a973f5302d03fd7746d4ae8085
hangyeol <snake011128@naver.com>
2025-04-08 11:09:07 +0900

    fix: 기존 환경변수 불러오던 부분 삭제

--- src/main/java/com/devpass/backend/BackendApplication.java
+++ src/main/java/com/devpass/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.devpass.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,18 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-            .directory("./")
-            .ignoreIfMissing()
-            .load();
-
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
-        System.setProperty("ELASTICSEARCH_USERNAME", dotenv.get("ELASTICSEARCH_USERNAME"));
-        System.setProperty("ELASTICSEARCH_PASSWORD", dotenv.get("ELASTICSEARCH_PASSWORD"));
-        System.setProperty("ELASTICSEARCH_URL", dotenv.get("ELASTICSEARCH_URL"));
 
         SpringApplication.run(BackendApplication.class, args);
     }
