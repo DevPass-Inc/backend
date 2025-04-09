878bbdc98ff7638ce9c42f34925401598d97485e
희은 <datakubit2@gmail.com>
2025-04-09 15:28:22 +0900

    refactor: 불필요한 테스트 파일 삭제

--- src/main/java/com/devpass/api/test/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devpass.api.test.repository;
-
-import com.devpass.api.test.domain.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
