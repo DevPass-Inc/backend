cdbc65206e2d1a7893a96cc3a07f96bd5e403978
희은 <datakubit2@gmail.com>
2025-04-23 00:40:20 +0900

    refactor: Swagger 설정 수정 / 로그인 유저 정보 주입용 @AuthUser 및 Interceptor 구현

--- src/main/java/com/devpass/global/config/SwaggerConfig.java
+++ src/main/java/com/devpass/global/config/SwaggerConfig.java
@@ -1,25 +1,38 @@
 package com.devpass.global.config;
 
 import io.swagger.v3.oas.models.Components;
+
+import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.devpass.global.annotation.AuthUser;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+    static {
+        SpringDocUtils.getConfig().addAnnotationsToIgnore(AuthUser.class);
     }
 
-    private Info apiInfo() {
-        return new Info()
-                .title("Devpass API")
-                .description("Devpass API Docs")
-                .version("1.0");
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+            .addList("bearerAuth");
+
+        return new OpenAPI()
+            .info(new Info().title("DevPass API").version("1.0"))
+            .addSecurityItem(securityRequirement)
+            .schemaRequirement("bearerAuth", securityScheme);
     }
 }
--- src/main/java/com/devpass/global/config/WebConfig.java
+++ src/main/java/com/devpass/global/config/WebConfig.java
@@ -1,15 +1,39 @@
 package com.devpass.global.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.devpass.global.constants.Constants;
+import com.devpass.global.interceptor.pre.AuthUserArgumentResolver;
+import com.devpass.global.interceptor.pre.JWTInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(authUserArgumentResolver);
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/swagger", "/swagger-ui/index.html");
         registry.addRedirectViewController("/swagger/", "/swagger-ui/index.html");
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new JWTInterceptor())
+            .addPathPatterns("/api/v1/**")
+            .excludePathPatterns(Constants.NO_NEED_FILTER_URLS);
     }
 }
