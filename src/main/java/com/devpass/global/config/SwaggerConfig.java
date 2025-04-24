b82b97c982efc31c8d5d055f305dc752c0b25ace
희은 <datakubit2@gmail.com>
2025-04-25 00:15:10 +0900

    refactor: Swagger에서 @AuthUser 파라미터 제외 처리

--- src/main/java/com/devpass/global/config/SwaggerConfig.java
+++ src/main/java/com/devpass/global/config/SwaggerConfig.java
@@ -15,6 +15,9 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
+    static {
+        SpringDocUtils.getConfig().addAnnotationsToIgnore(AuthUser.class);
+    }
 
     @Bean
     public OpenAPI openAPI() {
