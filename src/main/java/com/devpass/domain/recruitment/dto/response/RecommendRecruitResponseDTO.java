7f7785358fc7fc3f5fde2cb9d814beb3d267793e
hangyeol <snake011128@naver.com>
2025-05-01 15:28:13 +0900

    (hotfix): 기업 매칭시 채용공고 ID로 리턴하도록 수정

--- src/main/java/com/devpass/domain/recruitment/dto/response/RecommendRecruitResponseDTO.java
+++ src/main/java/com/devpass/domain/recruitment/dto/response/RecommendRecruitResponseDTO.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class RecommendRecruitResponseDTO {
 
+    private Long recruitmentId;
     private String companyName;
     private String position;
     private String finalScore;
