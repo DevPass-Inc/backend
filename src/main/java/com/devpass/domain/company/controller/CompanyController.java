eb568d2519a3edd9025d1a6a8703dca3a0ec5f4b
hangyeol <snake011128@naver.com>
2025-05-08 10:05:28 +0900

    fix: ElasticSearch를 사용하는 API 분리

--- src/main/java/com/devpass/domain/company/controller/CompanyController.java
+++ src/main/java/com/devpass/domain/company/controller/CompanyController.java
@@ -1,6 +1,5 @@
 package com.devpass.domain.company.controller;
 
-import com.devpass.domain.company.document.CompanyDocument;
 import com.devpass.domain.company.dto.response.CompanyDetailResponseDTO;
 import com.devpass.domain.company.service.CompanyService;
 import com.devpass.global.payload.ApiResponse;
@@ -8,13 +7,9 @@ import com.devpass.global.payload.apicode.SuccessCode;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,16 +21,8 @@ public class CompanyController {
 	@Operation(summary = "기업 조회", description = "기업 개별 조회")
 	@GetMapping("/{companyId}")
 	public ApiResponse<CompanyDetailResponseDTO> getCompanyById(@PathVariable Long companyId) {
-		CompanyDetailResponseDTO responseDto = companyService.getCompanyById(companyId);
+      CompanyDetailResponseDTO responseDto = companyService.getCompanyById(companyId);
 
-        return ApiResponse.of(SuccessCode.OK, responseDto);
-    }
-
-    @GetMapping("/")
-    public Page<CompanyDocument> searchCompanies(
-        @RequestParam String keyword, @PageableDefault(sort = "DESC") Pageable pageable) {
-        Page<CompanyDocument> companies = companyService.searchCompanies(keyword, pageable);
-
-        return ApiResponse.of(SuccessCode.OK, companies).getResult();
-    }
+      return ApiResponse.of(SuccessCode.OK, responseDto);
+  }
 }
